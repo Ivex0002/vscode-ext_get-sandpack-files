36c4a43e015a2282f1fadfc93537ffaf04396b8f
ivex0002 <garam199803@gmail.com>
2026-05-05 15:45:23 +0900

    edit : get file name

--- src/util/getFileNames.ts
+++ src/util/getFileNames.ts
@@ -1,12 +1,29 @@
 import { promises as fs } from "node:fs";
+import path from "node:path";
 
 export async function getFileNames(dir: string) {
   const entries = await fs.readdir(dir, {
     withFileTypes: true,
   });
 
-  return entries
-    .filter((v) => v.isFile())
-    .map((v) => v.name)
-    .sort();
+  const results: string[] = [];
+
+  for (const entry of entries) {
+    const fullPath = path.join(dir, entry.name);
+
+    if (entry.isFile()) {
+      results.push(entry.name);
+      continue;
+    }
+
+    if (entry.isDirectory()) {
+      const children = await getFileNames(fullPath);
+
+      for (const child of children) {
+        results.push(path.join(entry.name, child));
+      }
+    }
+  }
+
+  return results.sort();
 }
